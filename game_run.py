8697000a6769d98df1b415bf45fd18a700a01d9d
zakaria fahraoui <zakaria.fahraoui@gmail.com>
2021-01-27 08:36:15 +0100

    add enverment for agent

--- game_run.py
+++ game_run.py
@@ -28,8 +28,8 @@ STATES = {
 
 # the game's constant variables
 ball_radius = 10
-paddle_width = 80
-paddle_height = 10
+paddle_width = 300
+paddle_height = 20
 
 
 # paddle_max_vec = 15
@@ -43,7 +43,96 @@ class Brick:
         self.rect = pygame.Rect(self.x, self.y, block_width, block_height)
 
 
-class Breakout(object):
+class Environment:
+
+    def __init__(self):
+        self.bricks = []
+        self.ball_speed_y = 5
+        self.ball_speed_x = 3
+        self.ball_y = 450 - ball_radius
+        self.ball_x = 300
+        self.paddle_x = 470
+        self.paddle_y = 470
+        self.paddle_speed = 10
+        self.com_vec = 0
+        self.current_reward = STATES['Alive']
+        self.score = 0
+        self.ball_hit_count = 0
+        self.paddle_hit_count = 0
+        self.highScore = 0
+        self.isAuto = True
+        self.command = 0
+        self.iteration = 0
+        self.Q = np.zeros((int(1280 // resolution), int(480 // resolution), 3))
+        self.prev = [int((self.ball_x - self.paddle_x + 640) / resolution), int(self.ball_y / resolution)]
+
+    def decision(self):
+
+        # Observe what state is in and perform the action that maximizes expected reward.
+        actions = self.Q[int((self.ball_x - self.paddle_x + 640) / resolution), int(self.ball_y / resolution), :]
+
+        maxs = [i for i, x in enumerate(actions) if x == np.argmax(actions)]
+        if len(maxs) > 1:
+            if self.command in maxs:
+                com_command = self.command
+            else:
+                com_command = random.choice(maxs)
+        else:
+            com_command = np.argmax(actions)
+
+        if self.isAuto is True:
+            self.command = com_command
+
+        if self.command == 1:
+            # self.paddle_vec -= self.paddle_speed
+            self.paddle_x -= self.paddle_speed
+        elif self.command == 2:
+            # self.paddle_vec += self.paddle_speed
+            self.paddle_x += self.paddle_speed
+
+    def observe(self):
+        prev_Q = self.Q[self.prev[0], self.prev[1], self.command]
+
+        self.Q[self.prev[0], self.prev[1], self.command] = (
+                prev_Q + alpha * (self.current_reward + l *
+                                  max(self.Q[int(self.ball_x - self.paddle_x + 640 / resolution),
+                                      int(self.ball_y / resolution), :]) - prev_Q))
+        print(f"test {self.Q}")
+
+
+# class Agent:
+#     def __init__(self, environment):
+#         self.environment = environment
+#         self.policy = Policy(environment.states, ACTIONS)
+#         self.reset()
+
+
+# class Policy:
+#     def __init__(self):
+#         self.bricks = []
+#         self.ball_speed_y = 5
+#         self.ball_speed_x = 3
+#         self.ball_y = 450 - ball_radius
+#         self.ball_x = 300
+#         self.paddle_x = 470
+#         self.paddle_y = 470
+#         self.paddle_speed = 10
+#         self.com_vec = 0
+#
+#         self.score = 0
+#         self.ball_hit_count = 0
+#         self.paddle_hit_count = 0
+#         self.highScore = 0
+#         self.isAuto = True
+#         self.command = 0
+#         self.iteration = 0
+#         self.Q = np.zeros((int(1280 // resolution), int(480 // resolution), 3))
+#         self.screen = pygame.display.set_mode([640, 480])
+#
+#
+
+
+class Breakout:
 
     def __init__(self, myData):
 
@@ -52,7 +141,7 @@ class Breakout(object):
         self.ball_speed_x = 3
         self.ball_y = 450 - ball_radius
         self.ball_x = 300
-        self.paddle_x = 300
+        self.paddle_x = 470
         self.paddle_y = 470
         self.paddle_speed = 10
         self.com_vec = 0
@@ -111,12 +200,11 @@ class Breakout(object):
         # MOVE THE BALL
         self.ball_y += self.ball_speed_y
         self.ball_x += self.ball_speed_x
-
         self.hitDetect()
 
     def randomAngle(self):
         self.ball_y = 450 - ball_radius
-        ran = random.randint(0, 4)
+        ran = random.randint(-1, 0)
         self.ball_speed_x = (7 - ran) * self.ball_speed_x / abs(self.ball_speed_x)
         self.ball_speed_y = (3 + ran) * self.ball_speed_y / abs(self.ball_speed_y)
         self.ball_hit_count = 0
@@ -155,7 +243,6 @@ class Breakout(object):
             self.ball_x = self.screen.get_width() - ball_radius
             self.ball_speed_x = -self.ball_speed_x
 
-
         # for paddle
         if ball_rect.colliderect(paddle_rect):
             self.ball_speed_y = -self.ball_speed_y
@@ -168,7 +255,6 @@ class Breakout(object):
         # for bricks
         for brick in self.bricks:
             if brick.rect.colliderect(ball_rect):
-
                 self.score = self.score + 1
                 self.bricks.remove(brick)
                 self.ball_speed_y = - self.ball_speed_y
@@ -201,43 +287,6 @@ class Breakout(object):
 
         return True
 
-    def decision(self):
-        self.prev = [int((self.ball_x - self.paddle_x + 640) // resolution), int(self.ball_y // resolution)]
-
-        # Observe what state is in and perform the action that maximizes expected reward.
-        actions = self.Q[int((self.ball_x - self.paddle_x + 640) // resolution), int(self.ball_y // resolution), :]
-
-        maxs = [i for i, x in enumerate(actions) if x == np.argmax(actions)]
-        if len(maxs) > 1:
-            if self.command in maxs:
-                com_command = self.command
-            else:
-                com_command = random.choice(maxs)
-        else:
-            com_command = np.argmax(actions)
-
-        if self.isAuto is True:
-            self.command = com_command
-
-        if self.command == 1:
-            # self.paddle_vec -= self.paddle_speed
-            self.paddle_x -= self.paddle_speed
-        elif self.command == 2:
-            # self.paddle_vec += self.paddle_speed
-            self.paddle_x += self.paddle_speed
-        # else:
-        # if self.paddle_vec >0:
-        # self.paddle_vec -= self.paddle_speed
-        # elif self.paddle_vec < 0:
-        # self.paddle_vec += self.paddle_speed
-
-    def observe(self):
-        prev_Q = self.Q[self.prev[0], self.prev[1], self.command]
-
-        self.Q[self.prev[0], self.prev[1], self.command] = (
-                prev_Q + alpha * (self.current_reward + l *
-                                  max(self.Q[int(self.ball_x - self.paddle_x + 640 // resolution), int(self.ball_y // resolution), :]) - prev_Q))
-        print(f"test {self.Q}")
     def draw(self):
         self.screen.fill(white)
 
@@ -286,11 +335,12 @@ if len(sys.argv) > 1:
         print(s)
         game_run = Breakout(None)
 
+    environment = Environment()
     # game loop
     while game_run.input():
-        game_run.decision()
+        environment.decision()
         game_run.update()
-        game_run.observe()
+        environment.observe()
         game_run.draw()
 
     game_run.quit()
